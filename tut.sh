086e5679e6471fcce43267713551965c26bb1773
Jakub <jakubostrovvski@gmail.com>
2024-04-15 22:11:26 +0200

    tutorial

---
+++ tut.sh
@@ -0,0 +1,26 @@
+git commit
+
+git checkout -b <nazwa-gałęzi>
+
+git switch
+
+git merge
+
+git rebase -i 
+
+git branch -t/-f
+
+git log
+
+git tag
+
+git revert
+
+undo
+
+git reset
+
+git cherry-pick
+
+git commit --amend
+
